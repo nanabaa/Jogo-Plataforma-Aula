4d1ccd6ef1a376983c139ca42bf021f8a80f0770
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-02 10:09:34 -0300

    script player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private float horizontal;
+    private Rigidbody2D  rb;
+    private bool isFacingRight = true;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+        this.rb.velocity = new Vector2(horizontal * 8f, rb.velocity.y);
+        Flip();
+        /*if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Apertou espaço");
+        }
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            Debug.Log("Clicou com o botao direito");
+        }
+        */
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f) 
+        { 
+            isFacingRight = !isFacingRight;
+            Vector2 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
